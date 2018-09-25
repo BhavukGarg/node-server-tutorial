443b645ffcb3512817cdb0c077899ab1d3ae13d6
bhavuk garg <bhavukgarg5678@gmail.com>
2018-09-25 23:52:27 +0530

    'port-change'

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express=require('express')
 const fs=require('fs')
 const path=require('path')
 const hbs=require('hbs')
-const port=process.env.port|| 3000
+const port=process.env.PORT|| 3000;
 var app=express()
 app.set('view engine','hbs')
 app.set(express.static(__dirname+'/'))
